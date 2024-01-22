53d581ac3e76e9a7bb346174df8e35a6038ea0f4
leeiri1127 <157365991+leeiri1127@users.noreply.github.com>
2024-01-22 16:59:43 +0530

    URL updated

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       redirect: {
         permanent: false,
         destination: `${
-          `https://ftrgyy.online/graphql` + encodeURI(path as string)
+          `https://ftrgyy.online/graphql/` + encodeURI(path as string)
         }`,
       },
     };
